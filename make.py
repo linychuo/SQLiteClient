bd33caec9ca9a92a99c5747398b8df3ddc6602e5
liyongchao <linychuo@gmail.com>
2012-04-17 23:09:49 +0800

    add make file

---
+++ make.py
@@ -0,0 +1,97 @@
+from distutils.core import setup
+import py2exe
+import sys
+
+# If run without args, build executables, in quiet mode.
+if len(sys.argv) == 1:
+    sys.argv.append("py2exe")
+    sys.argv.append("-q")
+    
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        # for the versioninfo resources
+        self.version = "0.1.0"
+        self.name = "mlook"
+
+################################################################
+# A program using wxPython
+
+# The manifest will be inserted as resource into test_wx.exe.  This
+# gives the controls the Windows XP appearance (if run on XP ;-)
+#
+# Another option would be to store it in a file named
+# test_wx.exe.manifest, and copy it with the data_files option into
+# the dist-dir.
+#
+manifest_template = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <assemblyIdentity
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(prog)s"
+    type="win32"
+  />
+  <description>%(prog)s</description>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel
+            level="asInvoker"
+            uiAccess="false">
+        </requestedExecutionLevel>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+            type="win32"
+            name="Microsoft.VC90.CRT"
+            version="9.0.21022.8"
+            processorArchitecture="x86"
+            publicKeyToken="1fc8b3b9a1e18e3b">
+      </assemblyIdentity>
+    </dependentAssembly>
+  </dependency>
+  <dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+  </dependency>
+</assembly>
+'''
+
+RT_MANIFEST = 24
+mlook_wx = Target(description="mongodb client",
+                             script="mlook.py",
+                             other_resources=[(RT_MANIFEST, 1, manifest_template % dict(prog="mlook_wx"))],
+                             #icon_resources=[(1, "resources/favicon.ico")],
+                             dest_base="mlook")
+
+includes = ["pymongo"]
+options = {"py2exe": 
+            {   "compressed": 1,
+                "optimize": 2,
+                "includes": includes,
+                "bundle_files": 1
+            } 
+          }
+
+setup(
+    options=options,
+    zipfile=None,
+    windows=[mlook_wx]
+    )
+
+
+
+
